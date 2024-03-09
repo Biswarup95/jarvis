a0f7b62f939050f85738e70ee79f78e829db373b
Biswarup95 <biswarupkhatua95@gmail.com>
2024-03-09 11:03:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,103 @@
-import datetime
-
-import speech_recognition as sr
 import pyttsx3
-import pywhatkit
+import datetime
+import speech_recognition as sr
 import wikipedia
-import pyjokes
+import webbrowser
+import os
+import smtplib
 
-listener = sr.Recognizer()
 engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+#print(voices[1].id)
+engine.setProperty('voices', voices[0].id)
 
-
-def talk(text):
-    engine.say(text)
+def speak(audio):
+    engine.say(audio)
     engine.runAndWait()
 
-
-def take_commands():
-    try:
-        with sr.Microphone() as source:
-            print('listening....')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'jarvis' in command:
-                command = command.replace('jarvis', '')
-                print(command)
-
-    except:
-        pass
-    return command
-
-
-def run_jarvis():
-    command = take_commands()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play ', '')
-        talk('playing'+song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        print(time)
-        talk('Current time is'+time)
-    elif 'who is' in command:
-        person = command.replace('who is ', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        talk('sorry, I am busy')
-    elif 'are you single' in command:
-        talk('Depends, if you are ready to mingle, then yes')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")
     else:
-        talk('Please say the command again.')
+        speak("Good evening!")
+    speak("I am JARVIS. How may i help You")
 
+def takeCommand():
+    #microphone input
 
-while True:
-    run_jarvis()
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language="en-in")
+        print(f"User said: {query}\n")
+    except Exception as e:
+        print(e)
+        print("Say that again Please..")
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.login('biswarupkhatua95@gmail.com','bgmailk95')
+    server.sendmail('biswarupkhatua95@gmail.com', to, content)
+    server.close()
+
+if __name__ == '__main__':
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        #task execution
+        if 'wikipedia' in query:
+            speak("Searching wikipedia..")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            print(results)
+            speak(results)
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'open stack overflow' in query:
+            webbrowser.open("stackoverflow.com")
+        elif 'play music' in query:
+            '''
+            music_dir = 'path to music directory'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+            '''
+            pass
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            print(strTime)
+            speak(f"Sir, the time is{strTime}")
+        elif 'open IDE' in query:
+            '''
+            ide_path = 'path to exe file of ide'
+            os.startfile(ide_path)
+            '''
+            pass
+        elif 'send email' in query:
+            #google less secure app access option removed
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "biswarupkhatua95@yahoo.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry biswa bro. I am not able to send the mail.")
+        elif 'quit' in query:
+            exit()
